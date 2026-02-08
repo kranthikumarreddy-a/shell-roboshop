ddc65dadc554b7bc60ea4c6a6823642c34089aa9
kranthi <kranthireddy152@gmail.com>
2026-02-08 11:55:52 +0530

    is added

--- roboshop.sh
+++ roboshop.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
+set -e
+
 SG_ID="sg-019f25a2463a096b9"
 AMI_ID="ami-0220d79f3f480ecf5"
-HOST_ID="Z09442902NGE25Y6RCGF6"
 
-for instance in $@
+for instance in "$@"
 do
-INSTANCE_ID=$(aws ec2 run-instances \  
-               --image-id "$AMI_ID" \
-               --instance-type t3.micro \
-               --security-group-ids "$SG_ID" \
-               --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
-               --query 'Instances[0].InstanceId' \  # For each instance, we will get seperate uniqe ID
-               --output text)
-done      
-if [ $instance == frontend]; then
+  echo "Creating instance: $instance"
 
-        IP=$(
-             aws ec2 describe-instances \
-             --instance-ids "$INSTANCE_ID" \
-             --query 'Reservations[].Instances[].PublicIpAddress' \
-             --output text
-            )
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type t3.micro \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
 
+  echo "Instance ID: $INSTANCE_ID"
+
+  # wait until instance is running (important)
+  aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
+  if [ "$instance" = "frontend" ]; then
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query 'Reservations[].Instances[].PublicIpAddress' \
+      --output text)
   else
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query 'Reservations[].Instances[].PrivateIpAddress' \
+      --output text)
+  fi
 
-        IP=$(
-             aws ec2 describe-instances \
-             --instance-ids "$INSTANCE_ID" \
-             --query 'Reservations[].Instances[].PrivareIpAddress' \
-             --output text
-            )
-      fi      
-
-      echo $AMI_ID
-      echo $SG_ID
-      echo "Instance IP: $IP"
+  echo "Instance ($instance) IP: $IP"
 done
